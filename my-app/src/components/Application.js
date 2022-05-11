523f354a7c6b3f9a264fde51c590b8444f633ddc
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-05-11 09:17:20 -0400

    initial app form

--- my-app/src/components/Application.js
+++ my-app/src/components/Application.js
@@ -4,7 +4,16 @@ import React from 'react'
 function Application() {
   return (
     <div>
-      Application
+      <div className="application-form">
+      <h2>Krusty Krab Application Form</h2>
+      <form >
+        <input type="text" name="firstName" placeholder="First Name" />
+        <input type="text" name="lastName" placeholder="Last Name" />
+        <input type="text" name="experience"  placeholder="Previous Experience" />
+        <input type="text" name="reccomendation" placeholder="Reccomendations" />
+        <button type="submit">Submit Application</button>
+      </form>
+    </div>
     </div>
   )
 }
