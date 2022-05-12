688844b32a16cf7c14454be1620502958f278c7d
ChristianFish <cfishmusic@hotmail.com>
2022-05-12 09:50:08 -0500

    add/remove button

--- my-app/src/components/ThanksCard.js
+++ my-app/src/components/ThanksCard.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 
 const ThanksCard = ({ receipt }) => {
+
+  const handleClick = (e) => {
+    console.log(e)
+  }
     
   return (
     <div className="item">
       <p className="receiptname">{receipt.name}</p>
-      <p className="receiptprice">Price: ${receipt.price}</p>
+      <p className="receiptprice">Price: ${receipt.price} <button onClick={handleClick}>Remove</button></p>
     </div>
   )
 }
