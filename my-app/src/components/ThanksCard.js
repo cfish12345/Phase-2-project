c29cf9a33b8090f67160cd4a73fff45ef5da4feb
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-05-13 12:50:45 -0400

    remove delete button

--- my-app/src/components/ThanksCard.js
+++ my-app/src/components/ThanksCard.js
@@ -5,7 +5,7 @@ const ThanksCard = ({ receipt }) => {
   return (
     <div className="thanksCard">
       <p className="receiptname">{receipt.name}</p>
-      <p className="receiptprice">Price: ${receipt.price} <button className='primary'>Remove</button></p>
+      <p className="receiptprice">Price: ${receipt.price} </p>
     </div>
   )
 }
